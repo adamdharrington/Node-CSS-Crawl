5fe60bf460d4f3ef96a8c3f342762c89958fb5c3
adamdharrington <adamdharrington@gmail.com>
2014-07-23 13:14:52 +0100

    rewrote tests... grrrr

--- test/spec_lib_get-sources-list.js
+++ test/spec_lib_get-sources-list.js
@@ -1,84 +1,93 @@
 var expect = require("chai").expect,
   getList  = require("../lib/get-sources-list.js");
 
-describe("test sampling from top",function(){
-  var o;
-  before("setup globals",function(done){
-    GLOBAL.Opts = {
-      samples       : 1,
-      sampleSize   : 1,
-      sampleMethod : "TOP"
-    };
-    var d = function(err, obj){
-      if(err) throw err;
-      o = obj;
+describe("Parsing CSV file to common object: ",function(){
+  describe("Test sampling from TOP: ",function(){
+    var o;
+    before("setup globals",function(done){
+      GLOBAL.Opts = {
+        samples       : 1,
+        sampleSize   : 1,
+        sampleMethod : "TOP"
+      };
+      var d = function(err, obj){
+        if(err) throw err;
+        o = obj;
+        done();
+      };
+      getList.formatLists("test/lib/sampledata.csv", d);
+    });
+    it("Format of object",function(done){
+      expect(o).to.have.property("samples");
+      expect(o).to.have.property("sampling info");
+      expect(o["sampling info"]).to.have.property("sample size");
+      expect(o["sampling info"]).to.have.property("sample count");
+      expect(o["sampling info"]).to.have.property("sample method");
       done();
-    };
-    getList.formatLists("test/lib/sampledata.csv", d);
-  });
-
-  it("Correct number of results",function(done){
-    expect(o.sample1.length).to.equal(Opts.sampleSize);
-    done();
-  });
-  it("top result is 'google'",function(done){
-    expect(o.sample1[0]).to.equal("google.com");
-    done();
-  });
-});
-
-describe("test sampling from mid",function(){
-  var o;
-  before("setup globals",function(done){
-    GLOBAL.Opts = {
-      samples       : 1,
-      sampleSize   : 1,
-      sampleMethod : "MID"
-    };
-    var d = function(err, obj){
-      if(err) throw err;
-      o = obj;
+    });
+    it("Correct number of results",function(done){
+      expect(o.samples.sample1.length).to.equal(Opts.sampleSize);
       done();
-    };
-    getList.formatLists("test/lib/sampledata.csv", d);
-  });
-
-  it("Correct number of results",function(done){
-    expect(o.sample1.length).to.equal(Opts.sampleSize);
-    done();
-  });
-  it("MID result is 'yahoo.co.jp'",function(done){
-    expect(o.sample1[0]).to.equal("yahoo.co.jp");
-    done();
-  });
-});
-
-describe("test sampling from RANDOM",function(){
-  var o;
-  before("setup globals",function(done){
-    GLOBAL.Opts = {
-      samples       : 1,
-      sampleSize   : 1,
-      sampleMethod : "RANDOM"
-    };
-    var d = function(err, obj){
-      if(err) throw err;
-      o = obj;
+    });
+    it("top result is 'google'",function(done){
+      expect(o.samples.sample1[0]["host name"]).to.equal("google.com");
       done();
-    };
-    getList.formatLists("test/lib/sampledata.csv", d);
+    });
   });
 
-  it("Correct number of results",function(done){
-    expect(o.sample1.length).to.equal(1);
-    done();
+  describe("Test sampling from MID: ",function(){
+    var o;
+    before("setup globals",function(done){
+      GLOBAL.Opts = {
+        samples       : 1,
+        sampleSize   : 1,
+        sampleMethod : "MID"
+      };
+      var d = function(err, obj){
+        if(err) throw err;
+        o = obj;
+        done();
+      };
+      getList.formatLists("test/lib/sampledata.csv", d);
+    });
+
+    it("Correct number of results",function(done){
+      expect(o.samples.sample1.length).to.equal(Opts.sampleSize);
+      done();
+    });
+    it("MID result is 'yahoo.co.jp'",function(done){
+      expect(o.samples.sample1[0]["host name"]).to.equal("yahoo.co.jp");
+      done();
+    });
   });
-  it("Is a result with string type",function(done){
-    expect(typeof o.sample1[0]).to.equal("string");
-    done();
-  });
-  it("Is a non-empty result",function(done){
-    expect(o.sample1[0].length).to.be.above(1);
-    done();
+
+  describe("Test sampling from RANDOM: ",function(){
+    var o;
+    before("setup globals",function(done){
+      GLOBAL.Opts = {
+        samples       : 1,
+        sampleSize   : 1,
+        sampleMethod : "RANDOM"
+      };
+      var d = function(err, obj){
+        if(err) throw err;
+        o = obj;
+        done();
+      };
+      getList.formatLists("test/lib/sampledata.csv", d);
+    });
+
+    it("Correct number of results",function(done){
+      expect(o.samples.sample1.length).to.equal(1);
+      done();
+    });
+    it("Is a result with string type",function(done){
+      expect(typeof o.samples.sample1[0]["host name"]).to.equal("string");
+      done();
+    });
+    it("Is a non-empty result",function(done){
+      expect(o.samples.sample1[0]["host name"].length).to.be.above(1);
+      done();
+    });
   });
 });
--- test/spec_lib_http-requests.js
+++ test/spec_lib_http-requests.js
@@ -1,8 +1,25 @@
 var expect = require("chai").expect,
-    crawl  = require("../lib/http-requests.js");
+    g      = require("../lib/http-requests.js").getFile;
+    fs     = require("fs");
  
-describe('', function(){
-    it('should pass', function(){
-        expect(true).to.not.equal(false);
+describe('Test small http requests', function(){
+  var file = "http://assets.tumblr.com/images/default_avatar_40.png",
+      directory = "./test/data/";
+  before('do get and file write', function(done){
+    g(file,directory,function(err,dir){done();})
+  });
+  after('Remove directory and file',function(done){
+    fs.unlink(directory+"default_avatar_40.png", function(err){
+      if (err)throw err;
+      fs.rmdir(directory, function(err){
+        if (err)throw err;
+        done();
+      });
     });
+  });
+
+  it('Test that file was downloaded and written to disk', function(done){
+    expect(fs.existsSync(directory+"default_avatar_40.png")).to.equal(true);
+    done();
+  });
 });
